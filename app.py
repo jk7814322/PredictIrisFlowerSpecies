1bdbc67beaca34a15989ba781cc7f2417688ea49
Ashutosh010101 <106725688+Ashutosh010101@users.noreply.github.com>
2022-06-02 15:20:40 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+#app.py
+
+#import Flask
+from flask import Flask, render_template
+#create an instance of Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('home.html')
+if __name__ == '__main__':
+    app.run(debug=True)
