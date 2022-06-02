73ff76922024f223968927abe64a6d526333e0ef
Ashutosh010101 <106725688+Ashutosh010101@users.noreply.github.com>
2022-06-02 15:23:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #app.py
 
 #import Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 #create an instance of Flask
 app = Flask(__name__)
 @app.route('/')
@@ -9,3 +9,18 @@ def home():
     return render_template('home.html')
 if __name__ == '__main__':
     app.run(debug=True)
+    
+@app.route('/predict/', methods=['GET','POST'])
+def predict():
+    
+    if request.method == "POST":
+        
+        #get form data
+        sepal_length = request.form.get('sepal_length')
+        sepal_width = request.form.get('sepal_width')
+        petal_length = request.form.get('petal_length')
+        petal_width = request.form.get('petal_width')
+        
+        
+        return render_template('predict.html')
+    pass
